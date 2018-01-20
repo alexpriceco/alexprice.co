11dfbd55b348d5b0db35b18c148addff6c2dcce3
Alex Price <alex@alexprice.co>
2018-01-19 20:42:48 -0600

    Strand?

--- components/strand/strand.js
+++ components/strand/strand.js
@@ -7,8 +7,19 @@ export class Strand extends Component {
     return (
       <div className='canvas'>
         <svg width='800' height='600' viewBox='0 0 800 600'>
-          <path stroke='#2176FF' fill='none' transform='translate(116 98)' strokeWidth='4' d='M 62.0752 382.155C -20.4193 331.415 -20.9637 184 62.0752 134.155C 147.01 83.1726 185.013 286.349 281.075 262.155C 389.431 234.866 284.455 5.32908 396.075 0.15528C 478.995 -3.68823 515.996 64.453 561.075 134.155C 581.496 165.731 553.684 211.312 514.418 253.656C 478.317 292.587 418.576 244.745 396.075 196.655L 396.075 196.655C 356.575 112.234 239.68 253.863 176.032 341.802C 149.185 378.896 101.078 406.145 62.0752 382.155L 62.0752 382.155Z' />
+          <filter x='98' y='80' width='605.591' height='440.395' filterUnits='userSpaceOnUse' colorInterpolationFilters='sRGB'>
+            <feFlood floodOpacity='0' result='BackgroundImageFix' />
+            <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 0' />
+            <feOffset />
+            <feGaussianBlur stdDeviation='8' />
+            <feColorMatrix type='matrix' values='0 0 0 0 0.129412 0 0 0 0 0.462745 0 0 0 0 1 0 0 0 0.5 0' />
+            <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
+            <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
+          </filter>
+
+          <path stroke='#2176FF' strokeWidth='4' id='path1' transform='translate(116 98)' d='M 62.0752 382.155C -20.4193 331.415 -20.9637 184 62.0752 134.155C 147.01 83.1726 185.013 286.349 281.075 262.155C 389.431 234.866 284.455 5.32908 396.075 0.15528C 478.995 -3.68823 515.996 64.453 561.075 134.155C 609.892 209.638 435.178 466.424 383.075 365.155C 352.881 306.468 564.12 220.755 492.5 185.5C 390.465 135.273 263.002 420.752 133.575 403.655C 104.669 399.837 86.9109 397.431 62.0752 382.155Z' />
         </svg>
+
         <Stylesheet sheet={sheet} />
       </div>
     )
