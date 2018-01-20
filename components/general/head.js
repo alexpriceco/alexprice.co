22fd28c558f0206afee465149f277dcac0e80464
Alex Price <alex@alexprice.co>
2018-01-19 23:38:06 -0600

    Another favicon update :unamused:

--- components/general/head.js
+++ components/general/head.js
@@ -13,27 +13,14 @@ export const DocumentHead = (props) => (
     <meta property='og:url' content='https://alexprice.co/' />
     <meta property='og:description' content='Digital product designer from ATX. Background in electric vehicles, robotics, and supply chain edtech. D&D on the weekends.' />
 
-    <link rel='apple-touch-icon-precomposed' sizes='57x57' href='/static/favicons/apple-touch-icon-57x57.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='114x114' href='/static/favicons/apple-touch-icon-114x114.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='72x72' href='/static/favicons/apple-touch-icon-72x72.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='144x144' href='/static/favicons/apple-touch-icon-144x144.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='60x60' href='/static/favicons/apple-touch-icon-60x60.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='120x120' href='/static/favicons/apple-touch-icon-120x120.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='76x76' href='/static/favicons/apple-touch-icon-76x76.png' />
-    <link rel='apple-touch-icon-precomposed' sizes='152x152' href='/static/favicons/apple-touch-icon-152x152.png' />
-    <link rel='icon' type='image/png' href='/static/favicons/favicon-196x196.png' sizes='196x196' />
-    <link rel='icon' type='image/png' href='/static/favicons/favicon-96x96.png' sizes='96x96' />
-    <link rel='icon' type='image/png' href='/static/favicons/favicon-32x32.png' sizes='32x32' />
-    <link rel='icon' type='image/png' href='/static/favicons/favicon-16x16.png' sizes='16x16' />
-    <link rel='icon' type='image/png' href='/static/favicons/favicon-128.png' sizes='128x128' />
-    <meta name='application-name' content='Alex Price, Digital Product Designer' />
-    <meta name='msapplication-TileColor' content='#000000' />
-    <meta name='msapplication-TileImage' content='/static/favicons/mstile-144x144.png' />
-    <meta name='msapplication-square70x70logo' content='/static/favicons/mstile-70x70.png' />
-    <meta name='msapplication-square150x150logo' content='/static/favicons/mstile-150x150.png' />
-    <meta name='msapplication-wide310x150logo' content='/static/favicons/mstile-310x150.png' />
-    <meta name='msapplication-square310x310logo' content='/static/favicons/mstile-310x310.png' />
-
+    <link rel='apple-touch-icon' sizes='180x180' href='/static/favicons/apple-touch-icon.png' />
+    <link rel='icon' type='image/png' sizes='32x32' href='/static/favicons/favicon-32x32.png' />
+    <link rel='icon' type='image/png' sizes='16x16' href='/static/favicons/favicon-16x16.png' />
+    <link rel='manifest' href='/static/favicons/manifest.json' />
+    <link rel='mask-icon' href='/static/favicons/safari-pinned-tab.svg' color='#000000' />
+    <link rel='shortcut icon' href='/static/favicons/favicon.ico' />
+    <meta name='msapplication-config' content='/static/favicons/browserconfig.xml' />
+    <meta name='theme-color' content='#ffffff' />
   </Head>
 )
 
