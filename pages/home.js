0dfa00543661e4723cfa851e8e9a1f231579b447
Alex Price <alex@alexprice.co>
2018-09-19 12:18:01 -0500

    Home page is now contextually relevant

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Head from '../components/general/head.js'
-import Loader from '../components/loader/loader.js'
 import Stylesheet from '../components/general/stylesheet.js'
 import sheet from '../components/simple.scss'
 import axios from 'axios'
@@ -12,46 +11,45 @@ export default class Home extends Component {
       noToken: false,
       loading: true,
       today: new Date(),
-      aTask: {},
+      working: false,
       tasks: [],
-      date: '',
-      due: 0,
-      od: 0
+      date: ''
     }
   }
 
   componentDidMount () {
-    this.setState({ date: this.getDate() })
+    const now = new Date()
+    const isWorkDay = now.getDay() > 0 && now.getDay() < 6
+    const isWorkHours = now.getHours() >= 8 && now.getHours() < 17
+
     this.fetchTasks()
+    this.setState({
+      date: this.getDate(),
+      working: isWorkDay && isWorkHours
+    })
   }
 
   getDate () {
     const today = new Date()
     const month = (() => {
       switch (today.getMonth()) {
-        case 0: return 'Jan'
-        case 1: return 'Feb'
-        case 2: return 'Mar'
-        case 3: return 'Apr'
+        case 0: return 'January'
+        case 1: return 'February'
+        case 2: return 'March'
+        case 3: return 'April'
         case 4: return 'May'
-        case 5: return 'Jun'
-        case 6: return 'Jul'
-        case 7: return 'Aug'
-        case 8: return 'Sep'
-        case 9: return 'Oct'
-        case 10: return 'Nov'
-        case 11: return 'Dec'
+        case 5: return 'June'
+        case 6: return 'July'
+        case 7: return 'August'
+        case 8: return 'September'
+        case 9: return 'October'
+        case 10: return 'November'
+        case 11: return 'December'
         default: return ''
       }
     })()
 
-    const date = today.getDate()
-    return `${month} ${date}`
-  }
-
-  getDay () {
-    const today = new Date()
-    return (() => {
+    const day = (() => {
       switch (today.getDay()) {
         case 0: return 'Sunday'
         case 1: return 'Monday'
@@ -63,6 +61,9 @@ export default class Home extends Component {
         default: return ''
       }
     })()
+
+    const date = today.getDate()
+    return `${day}, ${month} ${date}`
   }
 
   tryKey (token) {
@@ -98,80 +99,119 @@ export default class Home extends Component {
             'token': token,
             'filter': `due ${this.getDate()} | overdue`
           }
-        })
-        .then(response => {
-          let dueToday = 0
-          let overdue = 0
-
+        }).then(response => {
           for (let task of response.data) {
-            if (task.due.string === this.state.date) dueToday++
-            else overdue++
+            console.debug(task)
           }
 
-          const random = Math.floor(Math.random() * response.data.length)
-
           this.setState({
-            aTask: response.data[random],
             tasks: response.data,
-            dueToday: dueToday,
-            overdue: overdue,
             loading: false
           }, resolve())
-        })
-        .catch(error => {
-          this.setState({ loading: false, error }, reject(new Error(error)))
+        }).catch(error => {
+          this.setState(
+            { loading: false, error },
+            reject(new Error(error))
+          )
         })
       }
     })
   }
 
-  getTaskSummary () {
-    const { overdue, dueToday, aTask } = this.state
-    let suggestion = ''
-    let span = <span>It looks like you're done for the day! 🎉 {suggestion}</span>
+  renderNoToken () {
+    return (
+      <section style={{ fontFamily: 'Inconsolata, monospace' }}>
+        <input
+          autoFocus
+          ref={input => { this.input = input }}
+          placeholder='Todoist API token'
+          value={this.state.token}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') this.tryKey(event.target.value)
+          }}
+        />
+        <span style={{ opacity: 0.5, paddingLeft: '1em' }}>
+        Todoist.com > settings > integrations
+        </span>
+      </section>
+    )
+  }
 
-    if (overdue) {
-      suggestion = <a href={aTask.url} rel='Launch Todoist'>"{String(aTask.content).replace(/__|\*|\#|(?:\[([^\]]*)\]\([^)]*\))/gm, '$1')}"?</a> // eslint-disable-line
-      if (dueToday) span = <span>You have {dueToday === 1 ? 'one ' : dueToday} task{dueToday === 1 ? '' : 's'} due today, and {overdue} overdue. Why don't you start with {suggestion}</span>
-      else span = <span>There's nothing on the docket today, but there are {overdue} overdue. Why don't you start with {suggestion}</span>
-    } else if (dueToday) {
-      suggestion = <a href={aTask.url} rel='Launch Todoist'>"{String(aTask.content).replace(/__|\*|\#|(?:\[([^\]]*)\]\([^)]*\))/gm, '$1')}"?</a> // eslint-disable-line
-      span = <span>You have {dueToday === 1 ? 'one ' : dueToday} task{dueToday === 1 ? '' : 's'} due today. Why don't you start with {suggestion}</span>
+  renderTasks (tasks) {
+    const filteredTasks = this.state.working
+      ? tasks.filter((task) => task.project_id === 2180640125)
+      : tasks.filter((task) => task.project_id !== 2180640125)
+
+    let formattedList = filteredTasks.map((task) => {
+      const formattedTask = task.content
+        .replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/gm, (str) => {
+          console.info(str)
+          const url = str.match(/\(.*?\)/)[0].replace(/\)|\(/g, '')
+          const label = str.match(/\[(.*?)\]/)[0].replace(/\]|\[/g, '')
+          return `<a href='${url}' title=${label}>${label}</a>`
+        })
+
+      return (
+        <li key={task.id}>
+          <svg width='18' height='18' viewBox='0 0 18 18' fill='none'>
+            <rect
+              opacity='0.25'
+              width='18'
+              height='18'
+              rx='2'
+              fill='white'
+            />
+          </svg>
+
+          <span
+            style={{ opacity: 0.75 }}
+            dangerouslySetInnerHTML={{ __html: formattedTask }}
+          />
+        </li>
+      )
+    })
+
+    if (filteredTasks.length < tasks.length) {
+      const n = tasks.length - filteredTasks.length
+      const nonwork = this.state.working ? ' (non-work)' : ''
+
+      formattedList.push(
+        <li key={'remaining-tasks'}>
+          <svg width='18' height='18' viewBox='0 0 18 18' fill='none'>
+            <path
+              opacity='0.25'
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M8 8V1C8 0.447715 8.44772 0 9 0V0C9.55228 0 10 0.447715 10 1V8H17C17.5523 8 18 8.44772 18 9V9C18 9.55228 17.5523 10 17 10H10V17C10 17.5523 9.55228 18 9 18V18C8.44772 18 8 17.5523 8 17V10H1C0.447715 10 0 9.55228 0 9V9C0 8.44772 0.447715 8 1 8H8Z'
+              fill='white'
+            />
+          </svg>
+
+          <span style={{ opacity: 0.5 }}>
+            {`${n} more${nonwork} task${n > 1 ? 's' : ''}`}
+          </span>
+        </li>
+      )
     }
 
-    return span
+    return <ul>{formattedList}</ul>
   }
 
   render () {
     return (
-      <section className={this.state.loading ? 'loading' : ''}>
+      <section
+        style={{ backgroundColor: 'black' }}
+        className={this.state.loading ? 'loading' : ''}
+      >
         <Stylesheet sheet={sheet} />
-        <Head title='🏠 Home' />
-        { this.state.loading
-          ? <section style={{ fontFamily: 'Inconsolata, monospace' }}>
-            <Loader />
+        <Head title={this.state.date} />
+        { this.state.noToken
+          ? this.renderNoToken()
+          : <section>
+            <h1>{this.state.date}</h1>
+            {this.renderTasks(this.state.tasks)}
           </section>
-          : (this.state.noToken
-            ? <section style={{ fontFamily: 'Inconsolata, monospace' }}>
-              <input
-                autoFocus
-                ref={input => { this.input = input }}
-                placeholder='Todoist API token'
-                value={this.state.token}
-                onKeyDown={(event) => {
-                  if (event.key === 'Enter') this.tryKey(event.target.value)
-                }}
-              />
-              <span style={{ opacity: 0.5, paddingLeft: '1em' }}>
-              Todoist.com > settings > integrations
-              </span>
-            </section>
-            : <section>
-              <h1>Good day, Alexander.</h1>
-              <p>
-                It's {this.getDay()} {this.getDate()}. <span>{this.getTaskSummary()}</span>
-              </p>
-            </section>)}
+        }
       </section>
     )
   }
